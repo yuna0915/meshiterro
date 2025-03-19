ceacf10df6cf29bfc6de42ca6f877029a90a9487
EC2 Default User <ec2-user@ip-172-31-41-246.ap-northeast-1.compute.internal>
2025-03-19 14:32:11 +0000

    [Finish] 4-16

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,26 @@
 class UsersController < ApplicationController
+
   def show
-    @user = User.find(params[:id])
+    @user = User.find(params[:id]) 
     @post_images = @user.post_images    
   end
-
+  
   def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to edit_user_path(@user)
+    else
+      render :edit
+    end
+  end
+end
+
+private
+
+def user_params
+  params.require(:user).permit(:name, :profile_image)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
   devise_for :users
   root to: 'homes#top'
